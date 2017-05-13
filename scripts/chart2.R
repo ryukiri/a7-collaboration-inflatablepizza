456cef4ab96a6714e87ac1f403cb6a1e56012c63
Chevy <chevy.swanson@gmail.com>
2017-05-13 15:44:55 -0700

    created chart2

--- scripts/chart2.R
+++ scripts/chart2.R
@@ -5,5 +5,8 @@ library(ggplot2)
 # who use what operating system and their command line proficiency.
 
 Chart2 <- function(dataset){
-  
+  pie <- ggplot(data = dataset) +
+         geom_bar(mapping = aes(x = What.operating.system.do.you.typically.use., fill = Using.the.command.line...terminal)) +
+         ggtitle("Students using which operating system and their command line proficiency")
+  return(pie)
 }
