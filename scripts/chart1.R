e348918ebdd995818e9a41fd3d6cb9fe46db811d
ZKeiLin <zlin2016@uw.edu>
2017-05-14 15:51:03 -0700

    change Chart1 a bit

--- scripts/chart1.R
+++ scripts/chart1.R
@@ -9,7 +9,7 @@ Chart1 <- function(dataset){
   group <- dataset%>%group_by(Do.you.consider.yourself.)%>%summarise(count = n()) 
   
   #Draw a pie chart 
-  pie <- ggplot(group, aes(x = 1, y = count, fill = factor(group$Do.you.consider.yourself.))) +
+  pie <- ggplot(group, aes(x = factor(1), y = count, fill = factor(group$Do.you.consider.yourself.))) +
     geom_bar(width = 1, stat = 'identity' ) + 
     coord_polar(theta = "y")+
     ggtitle("Do you consider yourself as a dog person or a cat person?") 
