bb6ed55daf0a9037ecab7421855c80f8179686aa
Chevy <chevy.swanson@gmail.com>
2017-05-15 17:26:19 -0700

    fixed chart 2 formatting and titles

--- scripts/chart2.R
+++ scripts/chart2.R
@@ -5,8 +5,12 @@ library(ggplot2)
 # who use what operating system and their command line proficiency.
 
 Chart2 <- function(dataset){
-  pie <- ggplot(data = dataset) +
+  
+  adjusted.data <- dataset %>%
+                   filter(What.operating.system.do.you.typically.use. == "Mac" | What.operating.system.do.you.typically.use. == "Windows" | What.operating.system.do.you.typically.use. == "Linux")
+  pie <- ggplot(data = adjusted.data) +
          geom_bar(mapping = aes(x = What.operating.system.do.you.typically.use., fill = Using.the.command.line...terminal)) +
-         ggtitle("Students using which operating system and their command line proficiency")
+         ggtitle("Command line proficiency by OS") +
+         labs(x = "OS",y = "Number of Students", fill = "Command Line Proficiency")
   return(pie)
 }
