7a4fddd0c587d8d4cfa019a4732465bc431de54d
Chevy <chevy.swanson@gmail.com>
2017-05-15 19:48:17 -0700

    changed variable name

--- scripts/chart2.R
+++ scripts/chart2.R
@@ -8,9 +8,9 @@ Chart2 <- function(dataset){
   
   adjusted.data <- dataset %>%
                    filter(What.operating.system.do.you.typically.use. == "Mac" | What.operating.system.do.you.typically.use. == "Windows" | What.operating.system.do.you.typically.use. == "Linux")
-  pie <- ggplot(data = adjusted.data) +
+  command.line.chart <- ggplot(data = adjusted.data) +
          geom_bar(mapping = aes(x = What.operating.system.do.you.typically.use., fill = Using.the.command.line...terminal)) +
          ggtitle("Command line proficiency by OS") +
          labs(x = "OS",y = "Number of Students", fill = "Command Line Proficiency")
-  return(pie)
+  return(command.line.chart)
 }
