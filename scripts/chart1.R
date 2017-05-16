1977360163578d8785114f3154b72410d79bc5b5
Austin Quach <austinquachh@gmail.com>
2017-05-15 20:04:35 -0700

    Fixed chart 1

--- scripts/chart1.R
+++ scripts/chart1.R
@@ -11,7 +11,19 @@ Chart1 <- function(dataset){
     geom_bar(width = 1, stat = 'identity' ) + 
     coord_polar(theta = "y")+
     ggtitle("Do you consider yourself as a dog person or a cat person?")+
-    labs(x = "", y = "", fill = "Choice")
+    labs(x = "", y = "", fill = "Choice") + 
+    theme(axis.line=element_blank(),
+          axis.text.x=element_blank(),
+          axis.text.y=element_blank(),
+          axis.ticks=element_blank(),
+          axis.title.x=element_blank(),
+          axis.title.y=element_blank(),
+          panel.background=element_blank(),
+          panel.border=element_blank(),
+          panel.grid.major=element_blank(),
+          panel.grid.minor=element_blank(),
+          plot.background=element_blank())
+    
   return (pie)
 }
 
