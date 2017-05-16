fc0a9ce958483222bd234d9f16293853168a1f21
ZKeiLin <zlin2016@uw.edu>
2017-05-15 19:41:30 -0700

    change chart1

--- scripts/chart1.R
+++ scripts/chart1.R
@@ -3,15 +3,15 @@ library(ggplot2)
 
 # Create a pie chart about the question "Do you consider 
 # yourself as a dog person or a cat person :)
-
 Chart1 <- function(dataset){
   #Organize information
   group <- dataset%>%group_by(Do.you.consider.yourself.)%>%summarise(count = n()) 
-  
   #Draw a pie chart 
   pie <- ggplot(group, aes(x = factor(1), y = count, fill = factor(group$Do.you.consider.yourself.))) +
     geom_bar(width = 1, stat = 'identity' ) + 
     coord_polar(theta = "y")+
-    ggtitle("Do you consider yourself as a dog person or a cat person?") 
+    ggtitle("Do you consider yourself as a dog person or a cat person?")+
+    labs(x = "", y = "", fill = "Choice")
   return (pie)
 }
+
